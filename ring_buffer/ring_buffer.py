8a0659ac099215bdcf68c84934e40889133e13cc
“Josefia <josefia.magagnoli@gmail.com>
2019-07-26 12:10:58 -0400

    Ring buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,12 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    if self.current < self.capacity - 1:
+      self.current += 1
+    else:
+      self.current = 0
 
   def get(self):
-    pass
+    filtered = filter(lambda x: x != None, self.storage)
+    return list(filtered)
